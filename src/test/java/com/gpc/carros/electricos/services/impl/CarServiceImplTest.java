e1e79f2974fc8379840099189d1753fd93badbba
racarrascal <racarrascal@sprc.com.co>
2025-02-20 16:30:25 -0500

    Actualizar librerias

--- src/test/java/com/gpc/carros/electricos/services/impl/CarServiceImplTest.java
+++ src/test/java/com/gpc/carros/electricos/services/impl/CarServiceImplTest.java
@@ -16,7 +16,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
